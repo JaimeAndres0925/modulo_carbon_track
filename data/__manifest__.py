abe2fd438654f553634e26f9d6f4ac7828b02c4f
JaimeAndres0925 <jaime.venegas@educa.madrid.org>
2026-04-22 11:43:34 +0200

    first commit

---
+++ data/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Carbon Track',
+    'version': '1.0',
+    'summary': 'Gestión de huella de carbono y emisiones CO2e',
+    'category': 'Sustainability', # Categoría alineada con tu Plan de Empresa [cite: 120]
+    'author': 'Jaime Andrés Venegas Cárdenas', # Según tu documento [cite: 5]
+    'depends': ['base', 'mail'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/carbon_track_menus.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
