2bd94ac51bebb7df75cab74af2e41691458804ee
JaimeAndres0925 <jaime.venegas@educa.madrid.org>
2026-04-24 16:13:57 +0200

    Menu del modelo de registro y breves comentarios

--- models/registro.py
+++ models/registro.py
@@ -4,13 +4,17 @@ class CarbonTrackRegistro(models.Model):
     _name = 'carbon.track.registro'
     _description = 'Registros de consumo (Datos de entrada)'
     
+    #Relaciones del modelo de datos
+    periodo_id = fields.Many2one(carbon.track.periodo, string= 'Periodo', required=True) [cite: 30]
     actividad_id = fields.Many2one('carbon.track.actividad', string='Actividad', required=True) [cite: 26]
+    
+    #Datos de entrada
     fecha = fields.Date(string='Fecha', default=fields.Fate.context_today) [cite: 27]
     cantidad = fields.Float(string='Cantidad', required=True) [cite: 28]
     unidad = fields.Char(string='Unidad', related='actividad_id_unidad',readonly=True) [cite: 29]
-    periodo_id = fields.Many2one(carbon.track.periodo, string= 'Periodo', required=True) [cite: 30]
     notas =fields.Text(string='Notas') [cite: 30]
     
+    # Campo calculado para el resultado
     valor_co2e = fields.Float(string='Total c02E (kg)', compute='_compute-co2e', store=True) [cite: 35]
     
     @api.depends('cantidad', 'actividad_id.factor_emision_id.valor')
